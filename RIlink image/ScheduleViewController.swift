ffd39629e765d54c3b76fe7586a56a149cae6732
Hsi Chuan Wang <cool24583185@gmail.com>
2022-04-25 11:52:49 +0800

    no message

---
+++ RIlink image/ScheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScheduleViewController.swift
+//  Rilink
+//
+//  Created by Jotangi on 2022/1/5.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
