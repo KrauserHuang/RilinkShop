968c79aa131e79200a8f25e0731f4b7bb5270d34
KrauserHuang <crescentdence@gmail.com>
2023-03-28 09:55:28 +0800

    [add] - add check/unchech icon for gender male/female checkbox switch

---
+++ RilinkShop/Extension/AnyObject + Ext.swift
@@ -0,0 +1,8 @@
+//
+//  AnyObject + Ext.swift
+//  RilinkShop
+//
+//  Created by Tai Chin Huang on 2023/3/25.
+//
+
+import Foundation
---
+++ RilinkShop/Extension/String + Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String + Ext.swift
+//  RilinkShop
+//
+//  Created by Tai Chin Huang on 2023/3/24.
+//
+
+import Foundation
